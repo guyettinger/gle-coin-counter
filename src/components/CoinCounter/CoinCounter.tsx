b71703a38710e15e5f230ce6e2847d1518076107
guyettinger <guy@pointivo.com>
2023-10-25 13:05:03 -0400

    RoboflowObjectDetectionCanvas and RoboflowWebcam

--- src/components/CoinCounter/CoinCounter.tsx
+++ src/components/CoinCounter/CoinCounter.tsx
@@ -1,19 +1,16 @@
-import { useRef, useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import styled from "styled-components"
-import { Button } from "gle-components";;
-import { useRoboflowClientContext } from "@/context/RoboflowClient/RoboflowClientContext";
-import { useActiveBreakpoints } from "@/hooks";
-import { asyncSetInterval } from "@/services/async/asyncService";
 import {
     RoboflowLoadParams,
     RoboflowModel,
     RoboflowObjectDetection
 } from "@/services/roboflowModule/roboflowModuleService.types";
-import { FACING_MODE_USER, VideoInputMode } from "@/services/mediaDevice/mediaDeviceService.types";
-import { getVideoInputModes } from "@/services/mediaDevice/mediaDeviceService";
-import { CoinCounterProps } from "@/components/CoinCounter/CoinCounter.types";
+import { useRoboflowClientContext } from "@/context/RoboflowClient/RoboflowClientContext";
+import { RoboflowWebcam } from "@/components/RoboflowWebcam";
+import { RoboflowObjectDetectionCanvas } from "@/components/RoboflowObjectDetectionCanvas";
 import { CoinCounterSummary } from "@/components/CoinCounterSummary/CoinCounterSummary";
+import { CoinCounterProps } from "@/components/CoinCounter/CoinCounter.types";
 
 const CoinCounterContainer = styled.div`
   display: flex;
@@ -31,273 +28,42 @@ const CoinCounterContent = styled.div`
   border-radius: 4px;
 `
 
-const CoinCounterToolbar = styled.div`
-  margin-bottom: 10px;
-`
-
 const CoinCounterVideoContent = styled.div`
   position: relative;
 `
 
-const CoinCounterWebcam = styled(Webcam)`
-  position: relative;
-  top: 0;
-  left: 0;
-`
-
-const CoinCounterCanvas = styled.canvas`
-  position: absolute;
-  top: 0;
-  left: 0;
-`
-
-const CoinCounterButton = styled(Button)`
-  float: right;
-`
-
-const CoinCounterLabel = styled.span`
-  font-size: 12px;
-  line-height: 1;
-`
-
-
 export const CoinCounter = ({coinCounterDetectionModel, coinCounterDetectionModelVersion}: CoinCounterProps) => {
     const webcamRef = useRef<Webcam>(null)
-    const canvasRef = useRef<HTMLCanvasElement>(null)
     const [objectDetections, setObjectDetections] = useState<RoboflowObjectDetection[]>([])
-    const [videoInitialized, setVideoInitialized] = useState<boolean>(false)
-    const [videoInputModes, setVideoInputModes] = useState<VideoInputMode[]>([])
-    const [videoInputMode, setVideoInputMode] = useState<VideoInputMode | null>(null)
+    const [webcamInitialized, setWebcamInitialized] = useState<boolean>(false)
+    const [webcamWidth, setWebcamWidth] = useState(0)
+    const [webcamHeight, setWebcamHeight] = useState(0)
     const roboflowClient = useRoboflowClientContext()
-
-    // determine screen size
-    const {isXs, isSm, isMd, isLg, isXl, isXxl} = useActiveBreakpoints()
-
-    // determine optimal video constraints dimensions
-    let constraintWidth = 640
-    let constraintHeight = 480
-
-    if (isXs || isSm) {
-        constraintWidth = 320
-        constraintHeight = 240
-    } else if (isMd || isLg) {
-        constraintWidth = 576
-        constraintHeight = 432
-    } else if (isXl || isXxl) {
-        constraintWidth = 640
-        constraintHeight = 480
-    }
-
-    // video constraints based on current video input mode
-    let videoConstraints: MediaTrackConstraints = {
-        facingMode: FACING_MODE_USER,
-        width: {max: constraintWidth},
-        height: {max: constraintHeight}
-    }
-
-    if (videoInputMode) {
-        videoConstraints.deviceId = videoInputMode.deviceId
-        videoConstraints.facingMode = videoInputMode.facingMode
-    }
-
-    // count of available video input modes
-    let videoInputModeCount = videoInputModes.length
-
-    const initializeVideo = () => {
-
-        let initializingVideo = false
-
-        let initializeInterval = asyncSetInterval(async () => {
-            const webcam = webcamRef?.current
-            if (!webcam) return
-
-            const video = webcam?.video
-            if (!video) return
-
-            const canvas = canvasRef?.current
-            if (!canvas) return
-
-            // check data is available
-            if (video.readyState !== 4) return
-
-            // return if already initialized
-            if (videoInitialized) return
-
-            // return if initializing
-            if (!initializingVideo) {
-                console.log("initializing video")
-                initializingVideo = true
-
-                // get all video input devices
-                const allVideoInputModes = await getVideoInputModes()
-
-                // default to first video input mode
-                const defaultVideoInputMode = allVideoInputModes[0]
-
-                // set all video input modes
-                console.log("initializing video input modes", allVideoInputModes)
-                setVideoInputModes(allVideoInputModes)
-
-                // set the default video input mode
-                console.log("initializing default video input mode", defaultVideoInputMode)
-                setVideoInputMode(defaultVideoInputMode)
-
-                // set video input modes initialized
-                console.log("initialized video")
-                setVideoInitialized(true)
-            }
-
-            // done initializing
-            clearInterval(initializeInterval)
-
-        }, 100);
-    }
+    const isReadyForCanvas = (webcamInitialized && webcamWidth > 0 && webcamHeight > 0)
 
     const detect = async (model: RoboflowModel) => {
+        if (!webcamInitialized) return
 
-        const webcam = webcamRef?.current
+        const webcam = webcamRef.current
         if (!webcam) return
 
-        const video = webcam?.video
+        const video = webcam.video
         if (!video) return
 
-        const canvas = canvasRef?.current
-        if (!canvas) return
-
-        // check data is available
-        if (video.readyState !== 4) return
-
-        const videoWidth = video.videoWidth
-        const videoHeight = video.videoHeight
-
-        video.width = videoWidth
-        video.height = videoHeight
-
-        // adjust the canvas size to match the video
-        adjustCanvas(videoWidth, videoHeight)
-
         //  get detections
-        const detections = await model.detect(video)
-        console.log('roboflow detected', detections)
-        setObjectDetections(detections)
-
-        const canvasContext = canvas.getContext("2d")
-        if (!canvasContext) return
-
-        drawBoxes(detections, canvasContext)
-    }
-
-    const adjustCanvas = (width: number, height: number) => {
-        const canvas = canvasRef?.current
-        if (!canvas) return
-
-        canvas.width = width * window.devicePixelRatio
-        canvas.height = height * window.devicePixelRatio
-
-        canvas.style.width = width + "px"
-        canvas.style.height = height + "px"
-
-        const canvasContext = canvas.getContext("2d")
-        if (!canvasContext) return
-        canvasContext.scale(window.devicePixelRatio, window.devicePixelRatio)
-    }
-
-    const drawBoxes = (detections: RoboflowObjectDetection[], canvasContext: CanvasRenderingContext2D) => {
-        const canvas = canvasRef?.current
-        if (!canvas) return
-
-        canvasContext.clearRect(0, 0, canvas.width, canvas.height)
-        detections.forEach((row: any) => {
-            //video
-            let temp = row.bbox
-            temp.class = row.class
-            temp.color = row.color
-            temp.confidence = row.confidence
-            row = temp
-
-            if (row.confidence < 0) return
-
-            //dimensions
-            let x = row.x - row.width / 2
-            let y = row.y - row.height / 2
-            let w = row.width
-            let h = row.height
-
-            //box
-            canvasContext.beginPath()
-            canvasContext.lineWidth = 1
-            canvasContext.strokeStyle = row.color
-            canvasContext.rect(x, y, w, h)
-            canvasContext.stroke()
-
-            //shade
-            canvasContext.fillStyle = "black"
-            canvasContext.globalAlpha = 0.2
-            canvasContext.fillRect(x, y, w, h)
-            canvasContext.globalAlpha = 1.0
-
-            //label
-            let fontColor = "black"
-            let fontSize = 12
-            canvasContext.font = `${fontSize}px monospace`
-            canvasContext.textAlign = "center"
-            let classTxt = row.class
-            let confTxt = (row.confidence * 100).toFixed().toString() + "%"
-            let msgTxt = classTxt + " " + confTxt
-            const textHeight = fontSize
-            let textWidth = canvasContext.measureText(msgTxt).width
-
-            if (textHeight <= h && textWidth <= w) {
-                canvasContext.strokeStyle = row.color
-                canvasContext.fillStyle = row.color
-                canvasContext.fillRect(
-                    x - canvasContext.lineWidth / 2,
-                    y - textHeight - canvasContext.lineWidth,
-                    textWidth + 2,
-                    textHeight + 1
-                );
-                canvasContext.stroke()
-                canvasContext.fillStyle = fontColor
-                canvasContext.fillText(msgTxt, x + textWidth / 2 + 1, y - 1)
-            } else {
-                textWidth = canvasContext.measureText(confTxt).width
-                canvasContext.strokeStyle = row.color
-                canvasContext.fillStyle = row.color
-                canvasContext.fillRect(
-                    x - canvasContext.lineWidth / 2,
-                    y - textHeight - canvasContext.lineWidth,
-                    textWidth + 2,
-                    textHeight + 1
-                )
-                canvasContext.stroke()
-                canvasContext.fillStyle = fontColor
-                canvasContext.fillText(confTxt, x + textWidth / 2 + 1, y - 1)
-            }
-        })
-    }
-
-    const toggleVideoMode = () => {
-        if (!videoInputModes) return
-        if (!videoInputMode) return
-        const videoInputModeIndex = videoInputModes.indexOf(videoInputMode)
-        let nextVideoInputModeIndex = videoInputModeIndex + 1
-        if (nextVideoInputModeIndex >= videoInputModes.length) {
-            nextVideoInputModeIndex = 0
+        try {
+            const detections = await model.detect(video)
+            console.log('roboflow detected', detections)
+            setObjectDetections(detections)
+        } catch (e) {
+            const error = e as Error
+            if(!error) return
+            console.error(error.message)
         }
-        const nextVideoInputMode = videoInputModes.at(nextVideoInputModeIndex)
-        if (!nextVideoInputMode) return;
-        setVideoInputMode(nextVideoInputMode)
-    }
-
-    const handleSwitchVideoModeClick = () => {
-        toggleVideoMode()
     }
 
     useEffect(() => {
-        if (!videoInitialized) {
-            // initialize
-            initializeVideo()
-        } else {
+        if (webcamInitialized) {
             // load the model
             const roboflowLoadParams: RoboflowLoadParams = {
                 model: coinCounterDetectionModel,
@@ -307,30 +73,37 @@ export const CoinCounter = ({coinCounterDetectionModel, coinCounterDetectionMode
                 // start inference
                 roboflowClient.startInference(detect)
             })
-
         }
-    }, [videoInitialized])
+    }, [webcamInitialized])
+
+    const handleRoboflowWebcamInitialized = () => {
+        setWebcamInitialized(true)
+        console.log('roboflow webcam initialized')
+    }
+
+    const handleRoboflowWebcamSizeChange = (width: number, height: number) => {
+        setWebcamWidth(width)
+        setWebcamHeight(height)
+        console.log('roboflow webcam size change', width, height)
+    }
 
     return (
         <CoinCounterContainer>
             <CoinCounterContent>
-                <CoinCounterToolbar>
-                    {videoInputMode && <CoinCounterLabel>{videoInputMode.label}</CoinCounterLabel>}
-                    {videoInputModeCount > 1 &&
-                        <CoinCounterButton variant={"small"} primary={true} onClick={handleSwitchVideoModeClick}>Switch
-                            camera</CoinCounterButton>}
-                </CoinCounterToolbar>
                 <CoinCounterVideoContent>
-                    <CoinCounterWebcam
+                    <RoboflowWebcam
                         ref={webcamRef}
-                        muted={true}
-                        width={constraintWidth}
-                        height={constraintHeight}
-                        videoConstraints={videoConstraints}
-                    />
-                    <CoinCounterCanvas
-                        ref={canvasRef}
-                    />
+                        onInitialized={handleRoboflowWebcamInitialized}
+                        onSizeChange={handleRoboflowWebcamSizeChange}
+                    >
+                        {isReadyForCanvas &&
+                            <RoboflowObjectDetectionCanvas
+                                width={webcamWidth}
+                                height={webcamHeight}
+                                objectDetections={objectDetections}
+                            />
+                        }
+                    </RoboflowWebcam>
                 </CoinCounterVideoContent>
                 {!!objectDetections && <CoinCounterSummary detections={objectDetections}/>}
             </CoinCounterContent>
